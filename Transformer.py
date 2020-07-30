cc0f7f11f900ddb2018c3f87bb35c6744a7a0680
ankit maurya <48839099+Ajax0564@users.noreply.github.com>
2020-07-30 19:09:49 +0530

    Add files via upload

---
+++ Transformer.py
@@ -0,0 +1,60 @@
+"papper link  https://arxiv.org/pdf/2001.08317"
+
+from multi_attention import SingleAttention,MultiAttention
+from time2vector import  Time2Vector
+import tensorflow as tf
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+import numpy as np
+
+
+batch_size = 32
+seq_len = 128
+
+d_k = 256
+d_v = 256
+n_heads = 12
+ff_dim = 256
+
+
+class TransformerEncoder(Layer):
+  def __init__(self, d_k, d_v, n_heads, ff_dim, dropout=0.1, **kwargs):
+    super(TransformerEncoder, self).__init__()
+    self.d_k = d_k
+    self.d_v = d_v
+    self.n_heads = n_heads
+    self.ff_dim = ff_dim
+    self.attn_heads = list()
+    self.dropout_rate = dropout
+
+  def build(self, input_shape):
+    self.attn_multi = MultiAttention(self.d_k, self.d_v, self.n_heads)
+    self.attn_dropout = Dropout(self.dropout_rate)
+    self.attn_normalize = LayerNormalization(input_shape=input_shape, epsilon=1e-6)
+
+    self.ff_conv1D_1 = Conv1D(filters=self.ff_dim, kernel_size=1, activation='relu')
+    # input_shape[0]=(batch, seq_len, 7), input_shape[0][-1] = 7 
+    self.ff_conv1D_2 = Conv1D(filters=input_shape[0][-1], kernel_size=1) 
+    self.ff_dropout = Dropout(self.dropout_rate)
+    self.ff_normalize = LayerNormalization(input_shape=input_shape, epsilon=1e-6)    
+  
+  def call(self, inputs): # inputs = (in_seq, in_seq, in_seq)
+    attn_layer = self.attn_multi(inputs)
+    attn_layer = self.attn_dropout(attn_layer)
+    attn_layer = self.attn_normalize(inputs[0] + attn_layer)
+
+    ff_layer = self.ff_conv1D_1(attn_layer)
+    ff_layer = self.ff_conv1D_2(ff_layer)
+    ff_layer = self.ff_dropout(ff_layer)
+    ff_layer = self.ff_normalize(inputs[0] + ff_layer)
+    return ff_layer 
+
+  def get_config(self): # Needed for saving and loading model with custom layer
+    config = super().get_config().copy()
+    config.update({'d_k': self.d_k,
+                   'd_v': self.d_v,
+                   'n_heads': self.n_heads,
+                   'ff_dim': self.ff_dim,
+                   'attn_heads': self.attn_heads,
+                   'dropout_rate': self.dropout_rate})
+    return config      
