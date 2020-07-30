615048f1466742c4d811b15deab2024106e4511d
ankit maurya <48839099+Ajax0564@users.noreply.github.com>
2020-07-30 19:08:24 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,43 @@
+from multi_attention import SingleAttention,MultiAttention
+from time2vector import  Time2Vector
+import tensorflow as tf
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+import numpy as np
+
+from Transformer import  TransformerEncoder
+
+
+
+batch_size = 32
+seq_len = 128
+
+d_k = 256
+d_v = 256
+n_heads = 12
+ff_dim = 256
+
+
+def create_model():
+  '''Initialize time and transformer layers'''
+  time_embedding = Time2Vector(seq_len)
+  attn_layer1 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+  attn_layer2 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+  attn_layer3 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+
+  '''Construct model'''
+  in_seq = Input(shape=(seq_len, 5))
+  x = time_embedding(in_seq)
+  x = Concatenate(axis=-1)([in_seq, x])
+  x = attn_layer1((x, x, x))
+  x = attn_layer2((x, x, x))
+  x = attn_layer3((x, x, x))
+  x = GlobalAveragePooling1D(data_format='channels_first')(x)
+  x = Dropout(0.1)(x)
+  x = Dense(64, activation='relu')(x)
+  x = Dropout(0.1)(x)
+  out = Dense(1, activation='linear')(x)
+
+  model = Model(inputs=in_seq, outputs=out)
+  model.compile(loss='mse', optimizer='adam', metrics=['mae', 'mape'])
+  return model
